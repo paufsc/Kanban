d1436bb45c85f86a1a0868c8e79eb80b601ad52b
Kemall Kanok <kemalkanok@gmail.com>
2014-10-26 22:58:24 +0200

    seed added

--- web/app/database/seeds/DatabaseSeeder.php
+++ web/app/database/seeds/DatabaseSeeder.php
@@ -10,7 +10,25 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+        role::insert("regular");
+        role::insert("admin");
+        permission::insert("sisteme giriş yapabilme");
+        permission::insert("görev ekleme");
+        permission::insert("görev durum değiştirme");
+        permission::insert("kişilere görev atama");
+        permission::insert("görev reddetme");
+        rolePermission::insert(1,1);
+        rolePermission::insert(1,3);
+        rolePermission::insert(1,5);
+        rolePermission::insert(2,1);
+        rolePermission::insert(2,2);
+        rolePermission::insert(2,3);
+        rolePermission::insert(2,4);
+        rolePermission::insert(2,5);
 
+
+
+        //User::create(array('email' => 'foo@bar.com'));
 		// $this->call('UserTableSeeder');
 	}
 
